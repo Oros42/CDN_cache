cdb2be04f7579c6303f47a80c3d0499d5b16d421
Oros42 <oros@ecirtam.net>
2014-01-25 12:32:19 +0100

    Init

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 CDN_cache
 =========
+  
+  
+/!\ Experimental code !  
+  
+On your computeur :  
+1- setup https-everywhere : https://www.eff.org/https-everywhere  
+2- move googleapis.xml to /home/<login>/.mozilla/firefox/<profile>/HTTPSEverywhereUserRules/  
+  
+On your server :  
+3- make the folder /mycdn  
+4- put index.php and .htaccess in /mycdn/  
+5- check that mod_rewrite is enabled  
+  
+6- go on http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js
+  If it work, you will be redirect to your server.
+s
---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+if(!empty($_GET['q'])){
+	$folder='./files/';
+	$q=$_GET['q'];
+
+	define('PHPPREFIX','<?php /* ');
+	define('PHPSUFFIX',' */ ?>');
+	define('DATASTORE','data.php');
+	$data=(file_exists(DATASTORE) ? unserialize(gzinflate(base64_decode(substr(file_get_contents(DATASTORE),strlen(PHPPREFIX),-strlen(PHPSUFFIX))))) : array() );
+	if($q=='list'){
+		if(!empty($data)){
+			foreach($data as $k => $d){
+				echo htmlentities($k).' : '.htmlentities($d).'<br/>';
+			}
+		}
+	}else{
+		if(!empty($data[$q]) && is_file($data[$q])){
+			echo file_get_contents($data[$q]);
+		}else{
+			$f=explode('/',$q);
+			switch($f[0]){
+				case 'ajax':
+					$q=substr($q,5);
+					$url='https://ajax.googleapis.com/ajax/libs/'.$q;
+					break;
+				case 'apis':
+					$q=substr($q,5);
+					$url='https://apis.google.com/js/'.$q;
+					break;
+				case 'themes':
+					$q=substr($q,7);
+					$url='https://themes.googleusercontent.com/'.$q;
+					break;
+				default:
+					exit();
+			}
+			$data[$q]=$folder.str_replace('/','__',$q).'.txt';
+			$file=file_get_contents($url);
+			file_put_contents($data[$q],$file);
+			file_put_contents(DATASTORE, PHPPREFIX.base64_encode(gzdeflate(serialize($data))).PHPSUFFIX);
+			echo $file;
+		}
+	}
+
+} ?>
